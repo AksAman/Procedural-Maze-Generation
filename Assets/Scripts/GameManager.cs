f1fd4275553f858bc136042fd98fb32fe4be4e37
AksAman <AksAman@github.com>
2018-07-07 22:54:35 +0530

    Plane boring maze done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazeprefab;
+	private Maze mazeInstance;
+
+	void Start()
+	{
+		BeginGame ();
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			RestartGame ();
+		}
+	}
+
+	void BeginGame ()
+	{
+		//Create a maze
+		Debug.Log ("New Game Started......!");
+		mazeInstance = Instantiate (mazeprefab) as Maze;
+
+		// Generate maze
+		mazeInstance.Generate ();
+	}
+
+	void RestartGame ()
+	{
+		Debug.Log ("Restarting......!");
+		Destroy (mazeInstance.gameObject);
+		BeginGame ();
+	}
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour {
+
+	// Needs a size
+	public int xSize, zSize;
+
+	//Needs reference to individual cell prefabs
+	public MazeCell cellPrefab;
+	private MazeCell[,] cells;
+
+	public void Generate()
+	{
+		cells = new MazeCell[xSize, zSize];
+		for (int x = 0; x < xSize; x++) {
+			for (int z = 0; z < zSize; z++) {
+				//Create cell - instantiate, name it, parent it, position it
+				CreateCell(x,z);
+			}
+		}
+	}
+
+	private void CreateCell(int x, int z)
+	{
+		MazeCell cellIns = Instantiate (cellPrefab) as MazeCell;
+		cellIns.name = "Cell " + x + " " + z;
+		cellIns.transform.parent = transform;
+		cellIns.transform.position = new Vector3 (x - xSize * 0.5f + 0.5f, 0, z - zSize * 0.5f + 0.5f);
+
+		cells [x, z] = cellIns;
+
+	}
+}
